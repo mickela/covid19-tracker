867f02de8b6463040367aef1cd6b558c400f76cc
Michael <osoba.michael@outlook.com>
2020-03-29 13:51:18 +0100

    modified spinner

--- src/components/spinner.jsx
+++ src/components/spinner.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Spinner(){
     return(
         <div className="text-center m-auto">
-            <img src="loader.gif" alt="Loading..."/>
+            <img src="loader.gif" className="loader" alt="Loading..."/>
         </div>
     )
 }
